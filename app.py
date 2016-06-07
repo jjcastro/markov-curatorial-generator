3e751d2b89227a38f6a1c498ee337e82b952ae53
J.J. Castro <jj.castro10@uniandes.edu.co>
2016-06-07 02:23:56 -0500

    Organization and cleaning up

--- app.py
+++ app.py
@@ -1,37 +1,62 @@
 import markovify
+import re
+
 from flask import Flask
+from flask import request
+
+
+# DEFAULT VARIABLES
+# ===========================
+
+default_num = 5;
+english_file = "./become-english.txt";
+spanish_file = "./become-spanish.txt";
+
+
+# LOGIC AND MARKOVIFY
+# ===========================
 
 app = Flask(__name__)
 
-# Get raw text as string.
-with open("./become-english.txt") as f:
+with open(english_file) as f:
     text_english = f.read()
-
-with open("./become-spanish.txt") as f:
+with open(spanish_file) as f:
     text_spanish = f.read()
 
-# Build the model.
 text_model_english = markovify.Text(text_english)
 text_model_spanish = markovify.Text(text_spanish)
 
 def get_sentences(n, text_model):
     string = ""
-    for i in range(5):
+    sentence_num = n if n is not None else default_num;
+    for i in range(sentence_num):
+        sentence = text_model.make_sentence()
+        if sentence is not None:
+            string += sentence
+    clean_str = re.sub(r'\.([A-Z])', r'. \1', string)
+    return clean_str
 
-        string += text_model.make_sentence()
-    return string
+
+# ROUTES
+# ===========================
 
 @app.route("/english")
 def english():
-    return get_sentences(5, text_model_english)
+    request_num = int(request.args.get('num'));
+    return get_sentences(request_num, text_model_english)
 
 @app.route("/spanish")
 def spanish():
-    return get_sentences(5, text_model_spanish)
+    request_num = int(request.args.get('num'));
+    return get_sentences(request_num, text_model_spanish)
 
 @app.route("/")
 def hello():
     return "Working"
+
+
+# MAIN
+# ===========================
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port = 5000)
